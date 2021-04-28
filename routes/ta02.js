7128217f2134a59f9bdf3a4fca319b2dbd826e54
alainluthier <alainluthier@gmail.com>
2021-04-28 15:52:46 -0400

    ta02

--- routes/ta02.js
+++ routes/ta02.js
@@ -3,13 +3,42 @@
 const express = require('express');
 const router = express.Router();
 
+const users = ['Adam','Mary','Michael'];
+var errors = '';
 router.get('/',(req, res, next) => {
-    res.render('pages', { 
+    res.render('pages/ta02', { 
         title: 'Team Activity 02', 
+        users:users,
+        error:errors,
         path: '/ta02', // For pug, EJS 
         activeTA03: true, // For HBS
         contentCSS: true, // For HBS
     });
+});
+
+router.post('/addUser', (req, res, next) => {
+    const username = req.body.username;
+    if(username!=''){
+        if(users.indexOf(username)==-1){
+            users.push(username);
+            errors='';
+        }else{
+            errors='Duplicated User';
+        }
+    }else{
+        errors='Empty User';
+    }   
+    res.redirect('/ta02');
+});
+router.post('/removeUser', (req, res, next) => {
+    const id =users.indexOf(req.body.username)
+    if (id!=-1){
+        errors='';
+        users.splice(id,1);
+    }else{
+        errors='User Name not found';
+    }
+    res.redirect('/ta02');
 });
 
 module.exports = router;
