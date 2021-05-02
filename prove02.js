fb86dcb23a61f0a9c9ba5a31707f0e2bfa4c6062
alainluthier <alainluthier@gmail.com>
2021-05-01 23:43:04 -0400

    final test

---
+++ prove02.js
@@ -0,0 +1,26 @@
+// Our initial setup (package requires, port number setup)
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const PORT = process.env.PORT || 5000 // So we can run on heroku || (OR) localhost:5000
+
+const app = express();
+
+// Route setup. You can implement more in the future!
+const ta01Routes = require('./routes/prove02');
+//const ta01Routes = require('./routes/ta01');
+
+app.use(express.static(path.join(__dirname, 'public')))
+   .set('views', path.join(__dirname, 'views'))
+   .set('view engine', 'ejs')
+   .use(bodyParser({extended: false})) // For parsing the body of a POST
+   .use('/ta01', ta01Routes)
+   .get('/', (req, res, next) => {
+     // This is the primary index, always handled last. 
+     res.render('pages/prove02', {title: 'Book and Summary', path: '/'});
+    })
+   .use((req, res, next) => {
+     // 404 page
+     res.render('pages/404', {title: '404 - Page Not Found', path: req.url})
+   })
+   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
---
+++ routes/prove02.js
@@ -0,0 +1,34 @@
+//TA02 PLACEHOLDER
+// Remember, you can make more of these placeholders yourself! 
+const express = require('express');
+const router = express.Router();
+
+var books = [
+    {name:"How to avoid a Climate Disaster",
+    summary:"How To Avoid A Climate Disaster is Bill Gates’ plea to the individuals, governments, and business leaders of the world to reduce greenhouse emissions by changing the way we make things, plug in, grow things, get around, and keep warm and cool.",
+    extraInfo:""
+    },
+    {name:"Breath",
+    summary:"Breath is a fascinating and helpful guide to understanding the science of breathing, including how doing it slowly and through your nose is best for your lungs and body, and the many proven mental and physical benefits of being more mindful of how you inhale and exhale.",
+    extraInfo:"The favorite quote from the author: \"By taking longer breaths, we allow our lungs to soak up more in fewer breaths\" "
+    },
+];
+var error='';
+router.get('/',(req, res, next) => {
+    res.render('pages/prove02', { 
+        title: 'Prove Activity 02', 
+        books:books,
+        path: '/prove02', // For pug, EJS 
+        activeTA03: true, // For HBS
+        contentCSS: true, // For HBS
+    });
+});
+router.post('/addBook',(req, res, next) => {
+    let name =  req.body.name;
+    let summary =  req.body.summary;
+    let extraInfo =  req.body.extrainfo;
+    books.push({name:name,summary:summary,extraInfo:extraInfo});
+    res.redirect('/prove02');
+});
+
+module.exports = router;
