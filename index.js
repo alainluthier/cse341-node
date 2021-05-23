c52b41bfefe4453eea9c5ea519d558d16fe67719
alainluthier <alainluthier@gmail.com>
2021-05-22 22:58:47 -0400

    final version login

--- index.js
+++ index.js
@@ -14,6 +14,10 @@
 const cors = require('cors') // Place this with other requires (like 'path' and 'express')
 const express = require('express');
 const bodyParser = require('body-parser');
+const csrf = require('csurf');
+const flash = require('connect-flash');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 const mongoose = require('mongoose');
 const path = require('path');
 const routes = require('./routes');
@@ -36,7 +40,11 @@ const options = {
 const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://webadmin:kjfF2foda16U4hAm@cluster0.vhpz6.mongodb.net/test?retryWrites=true&w=majority";
 
 const app = express();
-
+const store = new MongoDBStore({
+  uri: MONGODB_URL,
+  collection: 'sessions'
+});
+const csrfProtection = csrf();
 app.use(cors(corsOptions));
 
 
@@ -45,6 +53,16 @@ app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+   session({
+     secret: 'my secret',
+     resave: false,
+     saveUninitialized: false,
+     store: store
+   })
+ );
+ app.use(csrfProtection);
+ app.use(flash());
 app.use((req, res, next) => {
       User.findById('60a0956e5a14e34988130c26')
          .then(user => {
@@ -53,6 +71,11 @@ app.use((req, res, next) => {
          })
          .catch(err => console.log(err));
    });
+   app.use((req, res, next) => {
+      res.locals.isAuthenticated = req.session.isLoggedIn;
+      res.locals.csrfToken = req.csrfToken();
+      next();
+    });
 app.use('/', routes);
 mongoose.connect(MONGODB_URL,options).then(result => {
    User.findOne().then(user => {
--- routes/proveRoutes/index.js
+++ routes/proveRoutes/index.js
@@ -4,6 +4,7 @@ routes
     .use('/02',require('./prove02'))
     .use('/03',require('./prove03'))
     .use('/04',require('./prove04'))
+    .use('/05',require('./prove05'))
     /*.get('/',(req,res,next)=>{
         res.render('pages/proveActivities/',{
             pageTitle: 'Prove Activities',
